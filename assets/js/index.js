d516e2a8215b871b295dd93addf937849d88a9a2
Britt Carlton <brittanyrc95@gmail.com>
2022-10-08 14:54:04 -0400

    Implement update current weather section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,32 @@
 const OW_KEY = '0b5db2db41c74e8f32c3c74657289d6e';
+// TODO: remove these after development
+const TEST_COORDS = { lat: '20.35269', lon: '-77.30145', name: 'Test City' };
 
 // location form submit handler
 const handleLocationSubmit = async (e) => {
     e.preventDefault();
-
+    
     const location = document.querySelector('#loc-input').value.trim();
     
     // get location latitude/longitude
-    const coords = await getLocationCoords(location);
-
+    // const coords = await getLocationCoords(location);
+    
     // get current weather
-    const currentWeather = await getCurrentWeather(coords);
+    // TODO: replace lat and lon with location from line 10 after development
+    const currentWeather = await getCurrentWeather(TEST_COORDS);
     
     // use latitude/longitude to retrieve 5 day weather forecast
-    const forecast = await getForecast(coords);
-
-    // toggle welcome message display
+    const forecast = await getForecast(TEST_COORDS);
     
-    
-    // toggle weather forecast section
-    
+    // toggle off welcome message
+    document.querySelector('#welcome').classList = 'display-off';
+    document.querySelector('#forecast').classList = 'display-on';
     
     // update current forecast
-    
+    updateCurrentWeather(currentWeather);
 
     // create forecast cards for each day
-    
+    // updateForecast(forecast);
     
     // save current location to local storage
     
@@ -69,17 +70,38 @@ const getForecast = async (coords) => {
     return await makeAPICall(`https://api.openweathermap.org/data/2.5/forecast?lat=${coords.lat}&lon=${coords.lon}&units=imperial&appid=${OW_KEY}`);
 }
 
-// TODO: create function to toggle welcome message
+const updateCurrentWeather = (currentWeather) => {
+    const currentWeatherEl = document.querySelector('#current-weather');
 
+    // create elements to be added to currentWeatherEl
+    const locationEl = document.createElement('p');
+    const iconEl = document.createElement('img');
+    const tempEl = document.createElement('p');
+    const windEl = document.createElement('p');
+    const humidEl = document.createElement('p');
 
-// TODO: create function to toggle weather forecast section
+    // assign IDs/class names to elements
+    locationEl.classList = 'location-name';
+    iconEl.classList = 'weather-icon';
+    tempEl.classList = 'temp';
+    windEl.classList = 'wind';
+    humidEl.classList = 'humidity';
 
+    // add text to element innerHTML
+    locationEl.innerHTML = currentWeather.name;
+    iconEl.src = `http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`;
+    tempEl.innerHTML = currentWeather.main.temp;
+    windEl.innerHTML = currentWeather.wind.speed;
+    humidEl.innerHTML = currentWeather.main.humidity;
 
-// TODO: create function to update current weather section
-
+    // append children to currentWeatherEl
+    currentWeatherEl.append(locationEl, iconEl, tempEl, windEl, humidEl);
+}
 
 // TODO: create function to create forecast cards for each day
-
+const updateForecast = (forecast) => {
+    const futureForecastEl = document.querySelector('#5-day-forecast');
+}
 
 // TODO: create function to save data to local storage
 
